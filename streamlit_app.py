81276d82d9e324acb714403e93b3b88366196033
Pradeep Gadkari <64259538+pradeepgadkari@users.noreply.github.com>
2023-08-06 12:52:08 +0200

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healty Diner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
